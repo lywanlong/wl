2cb512f63c747670498c29b8c762f2264260d0c0
ldoubil <baikaiwen12@outlook.com>
2025-08-13 18:48:20 +0800

    213

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ wl = {}
 wl.version = 250813
 
 --- 向量类
-Vector2 = require 'wl.type.Vector2'
+Vector2 = require 'wl.type.Vector2'.create
 
 --- 引力场
 wl.force_field = require 'wl.object.runtime_object.force_field'
